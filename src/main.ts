35e82dfedfb6ab72bbcfd172a488db12b8bd090d
Edit Alatifi <editalatifi1996@gmail.com>
2026-02-08 23:11:26 +0100

    up

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000', // local dev
-      'https://crm-frontend-xi-three.vercel.app' // production frontend
-    ],
-    credentials: true,
-  });
+    app.enableCors({
+      origin: [
+        'http://localhost:3000',
+        'https://crm-frontend-xi-three.vercel.app',
+      ],
+      credentials: true,
+    });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT || 3001);;
 }
