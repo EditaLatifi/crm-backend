c73055da1989c6e15e6952e27b0ca0667138d8be
Edit Alatifi <editalatifi1996@gmail.com>
2026-02-08 23:55:09 +0100

    updates

--- src/common/prisma.service.ts
+++ src/common/prisma.service.ts
@@ -7,7 +7,20 @@ export class PrismaService
   implements OnModuleInit {
 
   async onModuleInit() {
-    await this.$connect();
+    let retries = 5;
+    while (retries) {
+      try {
+        await this.$connect();
+        break;
+      } catch (err) {
+        retries -= 1;
+        console.log(`Prisma connection failed, retrying (${5 - retries}/5)...`);
+        await new Promise(res => setTimeout(res, 5000)); // wait 5 seconds
+      }
+    }
+    if (!retries) {
+      throw new Error('Prisma could not connect after 5 retries');
+    }
   }
 
   async enableShutdownHooks(app: INestApplication) {
